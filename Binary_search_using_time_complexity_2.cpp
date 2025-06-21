61b36e9268fa32fbeda4e4c53413f8d1849e9618
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-06-22 03:43:13 +0600

    Binary Search using o(NlogN) time comeplexity

--- Binary_search_using_time_complexity_2.cpp
+++ Binary_search_using_time_complexity_2.cpp
@@ -34,9 +34,9 @@ Given 2 numbers N and Q, array A of N numbers and Q queries each one contains a 
 For each query print a single line that contains "found" if the number X exists in array A otherwise, print "not found".
 
 Input
-First line contains two numbers N, Q (1 ≤ N, Q ≤ 105).
-Second line contains N numbers (1 ≤ A, ≤ 10").
-Next Q lines contains X (1 ≤ X ≤ 10").
+First line contains two numbers N, Q (1 ≤ N, Q ≤ 10^5).
+Second line contains N numbers (1 ≤ A, ≤ 10^9).
+Next Q lines contains X (1 ≤ X ≤ 10^9).
 
 Output
 Print the answer for each query in a single line.
